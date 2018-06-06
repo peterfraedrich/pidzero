9e13e6b3ff7d8eb79d7819804b11842a5a697881
Peter Fraedrich <peter.fraedrich@capitalone.com>
2018-06-06 15:51:48 -0400

    frg105 push to branch master at Wed Jun  6 15:51:48 EDT 2018 (generated commit message)

--- Makefile
+++ Makefile
@@ -4,3 +4,8 @@ run:
 
 build-cpp:
 	haxe -main Pzd -cpp build/
+
+test:
+	haxe -main Pzd -cpp build/
+	cp build/Pzd .
+	./Pzd
--- pzd.hx
+++ pzd.hx
@@ -1,10 +1,18 @@
 // pidzero daemon
 // a docker process manager designed for docker
 
+// TODO: turn event loops into threads, because this shit is dumb AF
+
 import haxe.sys.*;
 import haxe.Timer;
 import haxe.*;
 import sys.*;
+#if cpp
+    import cpp.vm.Thread;
+#end
+#if neko
+    import neko.vm.Thread;
+#end
 
 class Pzd {
     // if name == main
@@ -14,20 +22,24 @@ class Pzd {
         var loop = new EventLoop(log);
         log.debug('Started pidzero');
         loop.start();
-        var mainloop = new Timer(1000);
-        mainloop.run = function () {
-            //do the thing
+        trace('main');
+        while (true) {
+            trace('true');
         }
     }
 }
 
+class Queue {
+    public static var outq:Array<Dynamic> = [];
+    public static var errq:Array<Dynamic> = [];
+}
+
 class EventLoop {
     public var log:Logger;
-    public var desired_state = {};
-    public var current_state = {};
+    public var procs:Array<ChildProcess> = [];
     public var loop = new Timer(100);
     private var counter:Int = 0;
-    private var daemons:Any;
+    private var daemons:Array<Dynamic>;
     private var firstrun:Bool = true;
 
     public function new(logger:Logger) : Void {
@@ -45,10 +57,6 @@ class EventLoop {
         this.log.debug('Event loop has been stopped.');
     }
 
-    public function status() : Bool {
-        return true;
-    }
-
     private function getDaemons() : Void {
         var f = sys.io.File.getContent('daemons.json');
         var units = haxe.Json.parse(f);
@@ -59,23 +67,68 @@ class EventLoop {
     private function runonce() : Void {
         this.getDaemons();
         this.spawn();
+        this.firstrun = false;
     }
 
     private function spawn() : Void {
         // spawn processes
-        
+        for (d in this.daemons) {
+            var p = new ChildProcess(d.name, d.command, d.vital, d.comments, d.environment, this.log);
+            var res = p.start();
+            if (res == true) {
+                this.procs.push(p);
+            } else {
+                this.log.error('Daemon ${d.name} did not start; failing.', 'class EventLoop() => method spawn()');
+            }
+        }
+    }
+
+    private function checkStatus() : Void {
+        /// check process status
+        for (p in this.procs) {
+            trace(p.rc);
+            if (p.rc != null) {
+                this.log.info('Daemon ${p.name} exited with status code ${p.rc}.');
+                if (p.vital == true) {
+                    this.log.error('Daemon ${p.name} is marked as vital:${p.vital} and existed with code ${p.rc}.');
+                }
+            }
+        }
+    }
+
+    private function getOutput() : Void {
+        // get process output
+        trace('go 1');
+        var out = Queue.outq.shift();
+        trace(out);
+        if (out != null) {
+            this.log.info(out[0], out[1]);
+        }
+        trace('go 2');
+        var err = Queue.errq.shift();
+        trace(err);
+        if (err != null) {
+            this.log.warn(err[0], err[1]);
+        }
+        trace('go 3');
+        return;
     }
 
     private function handle() : Void {
         // do the event loop things here
+        trace('*** START');
         if (this.firstrun == true) {
             this.runonce();
+            trace('run1');
         }
-        if (this.counter % 100 == 0) {
-            this.getDaemons();
-        }
+        this.checkStatus();
+        trace('checkStatus()');
+        this.getOutput();
+        trace('getOutput()');
 
         this.counter++;
+        trace('counter++');
+        trace('COUNTER: ${this.counter}');
     }
 }
 
@@ -105,7 +158,7 @@ class Logger {
         }
     }
 
-    private function log(loglevel:String, logtext:String, src:String='Main') : Void {
+    private function log(loglevel:String, logtext:String, src:String) : Void {
         //var msg = '${Date.now()} :: ${src} :: ${loglevel} :: ${logtext}';
         var msg = {
             timestamp : Date.now(),
@@ -122,22 +175,25 @@ class Logger {
             f.flush();
             f.close();
         }
+        if (loglevel == 'ERROR') {
+            throw msg;
+        }
     }
 
-    public function debug(text) : Void {
-        this.log('DEBUG', text);
+    public function debug(text:Null<String>=null, source:String='Main') : Void {
+        this.log('DEBUG', text, source);
     }
 
-    public function info(text) : Void {
-        this.log('INFO ', text);
+    public function info(text:Null<String>=null, source:String='Main') : Void {
+        this.log('INFO ', text, source);
     }
 
-    public function warn(text) : Void {
-        this.log('WARN ', text);
+    public function warn(text:Null<String>=null, source:String='Main') : Void {
+        this.log('WARN ', text, source);
     }
 
-    public function ERROR(text) : Void {
-        this.log('ERROR', text);
+    public function error(text:Null<String>=null, source:String='Main') : Void {
+        this.log('ERROR', text, source);
     }
 
 
@@ -149,37 +205,102 @@ class ChildProcess {
     public var command:String;
     public var vital:Bool;
     public var comments:String;
-    public var pidstatus:Bool;
-    public var pidrc:Int;
+    public var environment:Array<Dynamic>;
+    public var stdout:haxe.io.Input;
+    public var stderr:haxe.io.Input;
     private var p:sys.io.Process;
+    private var log:Logger;
+    public var rc:Null<Int> = null;
+    private var statusThread:Thread;
+    private var outThread:Thread;
+    private var errThread:Thread;
 
-    public function new(name:String, command:String, vital:Bool, comments:String) : Void {
+
+    public function new(name:String, command:String, vital:Bool, comments:String, environment:Array<Dynamic>, log:Logger) : Void {
         // return a new child process object
         this.name = name;
         this.command = command;
         this.vital = vital;
         this.comments = comments;
+        this.environment = environment;
+        this.log = log;
         return;
     }
 
     public function start() : Bool {
         // start the child process here
+        try {
+            this.p = new sys.io.Process(this.command);
+            this.stdout = this.p.stdout;
+            this.stderr = this.p.stderr;
+            this.pid = this.p.getPid();
+            this.statusThread = Thread.create(this.statusBlocking);
+            this.outThread = Thread.create(this.readStdout);
+            this.errThread = Thread.create(this.readStderr);
+        } catch (e:Dynamic) {
+            if (e) {
+                this.log.warn(e, 'class ChildProcess() -> method start() -> ${this.name}');
+                return false;
+            }
+        }
         return true;
     }
 
-    public function status() : Bool {
+    public function stop() : Void {
+        this.p.close();
+        return;
+    }
+
+    public function status() : Null<Int> {
         // check the status of the process here
-        return true;
+        return this.rc;
     }
 
-    public function rc() : Null<Int> {
-        // return returncode
-        return this.pidrc;
+    private function statusBlocking() : Void {
+        this.rc = this.p.exitCode();
+        return;
     }
 
-    public function output() : String {
-        // get the process output from stdout + stderr
-        return 'test';
+    private function readStdout() : Void {
+        var l = new Timer(100);
+        l.run = function () {
+            try {
+                var d = [
+                    'line' => this.stdout.readLine(),
+                    'source' => this.name
+                ];
+                trace(d);
+                Queue.outq.push(d);
+            } catch (e:Dynamic) {
+                if (e != 'Eof') {
+                    trace(e);
+                    this.log.error(e, this.name);
+                }
+            }
+        }
+        l.run();
+        return;
+    }
+
+    private function readStderr() : Void {
+        var l = new Timer(100);
+        l.run = function () {
+            try {
+                var d = [
+                    'line' => this.stderr.readLine(),
+                    'source' => this.name
+                ];
+                trace(d);
+                Queue.errq.push(d);
+            } catch (e:Dynamic) {
+                if (e != 'Eof') {
+                    trace(e);
+                    this.log.error(e, this.name);
+                }
+            }
+        }
+        l.run();
+        return;
     }
 
 }
